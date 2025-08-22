689d05406a5e41d3f771336181499090f2d945f0
nnakanghuy <huypk0607@gmail.com>
2025-08-22 14:51:29 +0700

    update bai1

--- bai1.cpp
+++ bai1.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hello world";
-    return 0;
-}
+// 26. Remove Duplicates from Sorted Array
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size()==0){
+            return 0;
+        }
+        int i=0;
+        for (int j=1;j<nums.size();j++){
+            if (nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+        cout<<"helo";
+    }
+};
