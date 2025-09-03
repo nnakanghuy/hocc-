459499db5a85bed503e284810ab239f5e9a41b8e
nnakanghuy <huypk0607@gmail.com>
2025-09-03 09:44:11 +0700

    update bai5 string

--- string_bai5.cpp
+++ string_bai5.cpp
@@ -1,46 +1,23 @@
 //5. Longest Palindromic Substring
 class Solution {
 public:
+string expand(string &s, int l, int r){
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                l--;
+                r++;
+            }
+            return s.substr(l+1,r - l -1);
+        }
     string longestPalindrome(string s) {
-        bool check;
         string kq="";
-        string temp;
-        int l, r;
-        if(s.size()==0){
-            return "";
-        }
-        if(s.size()==1){
-            return s;
-        }
-        if (s.size() == 2) {
-            if (s[0] == s[1]) return s;
-            else return s.substr(0,1);
-        }
-        for(int i=0;i<s.size()-1;i++){
-            for(int j=i+1;j<s.size();j++){
-                l=i;
-                r=j;
-                check=true;
-                while(l<r){
-                    if(s[l]==s[r]){
-
-                        l++;
-                        r--;
-                        check=true;
-                    } else{
-                        check=false;
-                        break;
-                    }
-                }
-                if (check== true){
-                    temp=s.substr(i,j-i+1);
-                    if(temp.size()>kq.size()){
-                        kq=temp;
-                        temp.clear();
-                    } else{
-                        temp.clear();
-                    }
-                }
+        for(int i=0;i<s.size();i++){
+            string sub1=  expand(s,i,i);
+            if(sub1.size()>kq.size()){
+                kq=sub1;
+            }
+            string sub2=expand(s,i,i+1);
+            if(sub2.size()>kq.size()){
+                kq=sub2;
             }
         }
         return kq;
