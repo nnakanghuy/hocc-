0e9ebe3c2c0b9598269d89e15ba57c415139fe88
nnakanghuy <huypk0607@gmail.com>
2025-09-07 09:29:39 +0700

    array_33

---
+++ array_33.cpp
@@ -0,0 +1,13 @@
+//33. Search in Rotated Sorted Array
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+                break;
+            }
+        }
+            return -1;
+    }
+};
