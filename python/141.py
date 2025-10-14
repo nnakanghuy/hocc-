3bb078334a14df0c92af31119fa8deb06f5638b8
nnakanghuy <huypk0607@gmail.com>
2025-10-14 22:33:16 +0700

    add

---
+++ python/141.py
@@ -0,0 +1,20 @@
+#141. Linked List Cycle
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        while fast and fast.next:
+            head = head.next
+            fast = fast.next.next
+            if head is fast:
+                return True
+        return False
